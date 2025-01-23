f2dd67ce8235df8ab634c843c91deca55ff050e8
Meryem sıla Fidan <silafidan40@gmail.com>
2025-01-23 17:17:45 +0300

    Queue

---
+++ Queue/src/Queue/QueueApp.java
@@ -0,0 +1,27 @@
+package Queue;
+
+public class QueueApp {
+    public static void main(String[] args) {
+        Queue theQueue =new Queue(5);
+
+
+        theQueue.insert(10);
+        theQueue.insert(20);
+        theQueue.insert(30);
+        theQueue.insert(40);
+
+        theQueue.remove();
+        theQueue.remove();
+
+        theQueue.insert(50);
+        theQueue.insert(60);
+
+
+
+        while (!theQueue.isEmpty()){
+            long n=theQueue.remove();
+            System.out.printf(n+ " ");
+        }
+        System.out.println("");
+    }
+}
